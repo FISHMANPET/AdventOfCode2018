53621463a4f754bce6c115403b3ac0e909d4417e
Peter Bajurny <FISHMANPET@gmail.com2>
2018-12-07 12:51:59 -0600

    d1p2
    well, it works, i guess

--- 1b.ps1
+++ 1b.ps1
@@ -1,1 +1,24 @@
-﻿
+﻿$thing = Get-Content .\input1.txt
+$output = 0
+$freqs = new-object System.Collections.ArrayList
+$i = 0
+while ($true) {
+    $index = $i%$($thing.Length)
+    $change = [int]$thing[$index]
+    $output+= $change
+    Write-Host "change: $change"
+    Write-Host "output: $output"
+    Write-Host "i:      $i"
+    write-host "index:  $index"
+    Write-Host "-------------"
+    if ($output -in $freqs) {
+        Write-Host "solution: $output"
+        break
+    }
+    $freqs.add($output)
+    $i++
+}
+#foreach ($line in $input) {
+#    $output += [int]$line
+#}
+#$output
