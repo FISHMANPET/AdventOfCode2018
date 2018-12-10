62e30919ff6bbea0d51314ffa510ee0df5a420d1
Peter Bajurny <FISHMANPET@gmail.com2>
2018-12-10 12:51:13 -0600

    day 2

---
+++ 2a.ps1
@@ -0,0 +1,16 @@
+﻿$thing = Get-Content .\input2.txt
+$numcontains2=0
+$numcontains3=0
+foreach ($line in $thing) {
+    $contains2 = $false
+    $contains3 = $false
+    foreach ($letter in [char[]]([int][char]'a'..[int][char]'z')) {
+
+        if (($line.ToCharArray() | Where-Object {$_ -eq $letter}).count -eq 2) {$contains2 = $true}
+        if (($line.ToCharArray() | Where-Object {$_ -eq $letter}).count -eq 3) {$contains3 = $true}
+    }
+    if ($contains2) { $numcontains2++}
+    if ($contains3) { $numcontains3++}
+}
+write-host "2: $numcontains2"
+write-host "3: $numcontains3"
---
+++ 2b.ps1
@@ -0,0 +1,28 @@
+﻿$thing = Get-Content .\input2.txt
+$sortedthing = $thing | Sort-Object
+$stringlength = $sortedthing[0].Length
+
+$firstchar = $thing | ForEach-Object {$_.Substring(1)} | Sort-Object | Get-Unique
+if ($firstchar.length -ne $sortedthing.Length) {
+    Write-Host "arrays differ, first character is difference"
+    break
+}
+$string1 = ""
+$string2 = ""
+
+foreach ($i in (0..($sortedthing.Length-2))) {
+    $differences = 0
+    foreach ($j in (0..($stringlength-1))) {
+        if ($sortedthing[$i][$j] -ne $sortedthing[$i+1][$j]) {$differences++}
+    }
+    if ($differences -eq 1) {
+        $string1 = $sortedthing[$i]
+        $string2 = $sortedthing[$i+1]
+        break
+    }
+}
+$string3 = ""
+foreach ($i in (0..($string1.Length-1))) {
+    if ($string1[$i] -eq $string2[$i]) {$string3+=$string1[$i]}
+}
+$string3
